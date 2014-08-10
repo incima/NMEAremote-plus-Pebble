34dbed009aaeaa557aaa08554dca4a9c484d1aad
Holger Franzen <holger.franzen@incima.net>
2014-08-10 22:22:27 +0200

    Fixed dot and layouts.

--- src/NMEAremote_pebble.c
+++ src/NMEAremote_pebble.c
@@ -1,18 +1,18 @@
 #include "pebble.h"
 
-
 #define FONT_OPENSANS_EXTRA_48_NUMBERS RESOURCE_ID_FONT_OPENSANS_EXTRA_52_NUMBERS
+#define FONT_OPENSANS_LIGHT_24_TEXT RESOURCE_ID_FONT_OPENSANS_LIGHT_24_TEXT
 #define FONT_OPENSANS_BOLD_24_NUMBERS RESOURCE_ID_FONT_OPENSANS_BOLD_24_NUMBERS
 #define FONT_OPENSANS_EXTRA_24_TEXT RESOURCE_ID_FONT_OPENSANS_EXTRA_24_TEXT
 
 static Window *window;
 
-static TextLayer *speed_layer;
 static char speed[16];
 static char hdg[16];
 static char awa[16];
 
-static Layer *circle_layer;
+static TextLayer *top_value_layer;
+static TextLayer *top_title_layer;
 
 static Layer *right_bg_layer;
 static TextLayer *right_value_layer;
@@ -21,6 +21,8 @@ static TextLayer *right_title_layer;
 static Layer *left_bg_layer;
 static TextLayer *left_value_layer;
 static TextLayer *left_title_layer;
+
+static Layer *circle_layer;
 
 static AppSync sync;
 static uint8_t sync_buffer[48];
@@ -57,7 +59,7 @@ static void circle_layer_update_proc(Layer *layer, GContext *context)
 		
 	GRect layer_frame = layer_get_bounds(layer);		
 	graphics_context_set_fill_color(context, color);			
-	graphics_fill_circle(context, (GPoint){layer_frame.size.w/2, layer_frame.size.h/2}, layer_frame.size.w/2);			
+	graphics_fill_circle(context, (GPoint){layer_frame.size.w/2, layer_frame.size.h/2}, 2);			
 	color = (color == GColorWhite ? GColorBlack : GColorWhite);
 }
 
@@ -70,7 +72,7 @@ static void rounded_layer_update_proc(Layer* layer, GContext *context)
 
 static void app_timer_callback(void *data) 
 {	
-	text_layer_set_text(speed_layer, speed);		
+	text_layer_set_text(top_value_layer, speed);		
   text_layer_set_text(left_value_layer, hdg);		
  	text_layer_set_text(right_value_layer, awa);
 	
@@ -83,11 +85,18 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
 
 	// Top layer
-  speed_layer = text_layer_create(GRect(0, 20, 144, 84));
-  text_layer_set_text_color(speed_layer, GColorWhite);
-  text_layer_set_background_color(speed_layer, GColorClear);
-  text_layer_set_font(speed_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_EXTRA_48_NUMBERS)));
-  text_layer_set_text_alignment(speed_layer, GTextAlignmentCenter);
+  top_value_layer = text_layer_create(GRect(0, 10, 144, 54));
+  text_layer_set_text_color(top_value_layer, GColorWhite);
+  text_layer_set_background_color(top_value_layer, GColorClear);
+  text_layer_set_font(top_value_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_EXTRA_48_NUMBERS)));
+  text_layer_set_text_alignment(top_value_layer, GTextAlignmentCenter);
+
+  top_title_layer = text_layer_create(GRect(0, 64, 144, 40));
+  text_layer_set_text_color(top_title_layer, GColorWhite);
+  text_layer_set_background_color(top_title_layer, GColorClear);
+  text_layer_set_font(top_title_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_LIGHT_24_TEXT)));
+  text_layer_set_text_alignment(top_title_layer, GTextAlignmentCenter);
+	text_layer_set_text(top_title_layer, "SPEED");
 
 	// Left layer
 	left_bg_layer = layer_create(GRect(0, 104, 70, 64));
@@ -124,14 +133,15 @@ static void window_load(Window *window) {
 	text_layer_set_text(right_title_layer, "AWA");
 	
 	GRect window_layer_frame = layer_get_frame(window_layer);			
-	circle_layer = layer_create(GRect(window_layer_frame.origin.x + window_layer_frame.size.w - 4, 
-																		window_layer_frame.origin.y, 
-																		4, 4));																		
+	circle_layer = layer_create(GRect(window_layer_frame.origin.x + window_layer_frame.size.w - 7, 
+																		window_layer_frame.origin.y + 1, 
+																		5, 5));																		
 	layer_set_update_proc(circle_layer, circle_layer_update_proc);	
   
 	layer_add_child(window_layer, left_bg_layer);
 	layer_add_child(window_layer, right_bg_layer);
-  layer_add_child(window_layer, text_layer_get_layer(speed_layer));
+  layer_add_child(window_layer, text_layer_get_layer(top_value_layer));
+	layer_add_child(window_layer, text_layer_get_layer(top_title_layer));	
   layer_add_child(window_layer, text_layer_get_layer(left_value_layer));
 	layer_add_child(window_layer, text_layer_get_layer(right_value_layer));	
   layer_add_child(window_layer, text_layer_get_layer(left_title_layer));
@@ -142,10 +152,11 @@ static void window_load(Window *window) {
 static void window_unload(Window *window) {
   app_sync_deinit(&sync);
 
-  text_layer_destroy(speed_layer);
+  text_layer_destroy(top_value_layer);
+  text_layer_destroy(top_title_layer);	
   text_layer_destroy(left_value_layer);
-  text_layer_destroy(right_value_layer);	
   text_layer_destroy(left_title_layer);
+  text_layer_destroy(right_value_layer);		
   text_layer_destroy(right_title_layer);	
 	layer_destroy(left_bg_layer);
 	layer_destroy(right_bg_layer);	
