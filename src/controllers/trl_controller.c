fc320c833fa304392058f49c801597c26a0ffac3
Holger Franzen <holger.franzen@incima.net>
2014-08-30 19:58:55 +0200

    Refactored trl layers

--- src/controllers/trl_controller.c
+++ src/controllers/trl_controller.c
@@ -21,6 +21,8 @@ void trl_controller_load(Controller* controller)
   Layer *window_layer = window_get_root_layer(trl_controller->controller.window);
 
 	// Top layer
+	trl_controller->top_layer = layer_create(GRect(0, 0, 144, 104));	
+	
   trl_controller->top_value_layer = text_layer_create(GRect(0, 10, 144, 54));
   text_layer_set_text_color(trl_controller->top_value_layer, GColorWhite);
   text_layer_set_background_color(trl_controller->top_value_layer, GColorClear);
@@ -35,12 +37,18 @@ void trl_controller_load(Controller* controller)
   text_layer_set_font(trl_controller->top_title_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_LIGHT_24_TEXT)));
   text_layer_set_text_alignment(trl_controller->top_title_layer, GTextAlignmentCenter);
 	text_layer_set_text(trl_controller->top_title_layer, "");
+	
+	layer_add_child(trl_controller->top_layer, text_layer_get_layer(trl_controller->top_value_layer));
+	layer_add_child(trl_controller->top_layer, text_layer_get_layer(trl_controller->top_title_layer));	
 		
-	// Left layer
-	trl_controller->left_bg_layer = layer_create(GRect(0, 104, 70, 64));
+	// bottom layer
+	trl_controller->bottom_layer = layer_create(GRect(0, 104, 144, 64));
+	
+	// Left bottom layer
+	trl_controller->left_bg_layer = layer_create(GRect(0, 0, 70, 64));
 	layer_set_update_proc(trl_controller->left_bg_layer, rounded_layer_update_proc);	
 
-  trl_controller->left_value_layer = text_layer_create(GRect(0, 106, 70, 24));
+  trl_controller->left_value_layer = text_layer_create(GRect(0, 0, 70, 24));
   text_layer_set_background_color(trl_controller->left_value_layer, GColorClear);
   text_layer_set_text_color(trl_controller->left_value_layer, GColorBlack);	
   text_layer_set_font(trl_controller->left_value_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_BOLD_24_NUMBERS)));
@@ -48,18 +56,22 @@ void trl_controller_load(Controller* controller)
 	text_layer_set_overflow_mode(trl_controller->left_value_layer, GTextOverflowModeWordWrap);	
 	text_layer_set_text(trl_controller->left_value_layer, "");
   
-	trl_controller->left_title_layer = text_layer_create(GRect(0, 133, 70, 37));
+	trl_controller->left_title_layer = text_layer_create(GRect(0, 27, 70, 37));
   text_layer_set_background_color(trl_controller->left_title_layer, GColorClear);
 	text_layer_set_text_color(trl_controller->left_title_layer, GColorBlack);
   text_layer_set_font(trl_controller->left_title_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_EXTRA_24_TEXT)));
   text_layer_set_text_alignment(trl_controller->left_title_layer, GTextAlignmentCenter);
 	text_layer_set_text(trl_controller->left_title_layer, "");
 	
-	// Right layer
-	trl_controller->right_bg_layer = layer_create(GRect(74, 104, 70, 64));
+	layer_add_child(trl_controller->bottom_layer, trl_controller->left_bg_layer);	
+	layer_add_child(trl_controller->bottom_layer, text_layer_get_layer(trl_controller->left_value_layer));
+	layer_add_child(trl_controller->bottom_layer, text_layer_get_layer(trl_controller->left_title_layer));	
+	
+	// Right bottom layer
+	trl_controller->right_bg_layer = layer_create(GRect(74, 0, 70, 64));
 	layer_set_update_proc(trl_controller->right_bg_layer, rounded_layer_update_proc);
 
-  trl_controller->right_value_layer = text_layer_create(GRect(74, 106, 70, 24));
+  trl_controller->right_value_layer = text_layer_create(GRect(74, 0, 70, 24));
   text_layer_set_background_color(trl_controller->right_value_layer, GColorClear);
 	text_layer_set_text_color(trl_controller->right_value_layer, GColorBlack);
   text_layer_set_font(trl_controller->right_value_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_BOLD_24_NUMBERS)));
@@ -67,22 +79,20 @@ void trl_controller_load(Controller* controller)
 	text_layer_set_overflow_mode(trl_controller->right_value_layer, GTextOverflowModeWordWrap);		
 	text_layer_set_text(trl_controller->right_value_layer, "");
 				
-  trl_controller->right_title_layer = text_layer_create(GRect(74, 133, 70, 37));
+  trl_controller->right_title_layer = text_layer_create(GRect(74, 27, 70, 37));
   text_layer_set_background_color(trl_controller->right_title_layer, GColorClear);
 	text_layer_set_text_color(trl_controller->right_title_layer, GColorBlack);
   text_layer_set_font(trl_controller->right_title_layer, fonts_load_custom_font(resource_get_handle(FONT_OPENSANS_EXTRA_24_TEXT)));
   text_layer_set_text_alignment(trl_controller->right_title_layer, GTextAlignmentCenter);
 	text_layer_set_text(trl_controller->right_title_layer, "");
-		
-	layer_add_child(window_layer, trl_controller->left_bg_layer);
-	layer_add_child(window_layer, trl_controller->right_bg_layer);
-  layer_add_child(window_layer, text_layer_get_layer(trl_controller->top_value_layer));
-	layer_add_child(window_layer, text_layer_get_layer(trl_controller->top_title_layer));	
-  layer_add_child(window_layer, text_layer_get_layer(trl_controller->left_value_layer));
-	layer_add_child(window_layer, text_layer_get_layer(trl_controller->right_value_layer));	
-  layer_add_child(window_layer, text_layer_get_layer(trl_controller->left_title_layer));
-	layer_add_child(window_layer, text_layer_get_layer(trl_controller->right_title_layer));		
 
+	layer_add_child(trl_controller->bottom_layer, trl_controller->right_bg_layer);	
+	layer_add_child(trl_controller->bottom_layer, text_layer_get_layer(trl_controller->right_value_layer));
+	layer_add_child(trl_controller->bottom_layer, text_layer_get_layer(trl_controller->right_title_layer));	
+
+	// add top, bottom to window
+  layer_add_child(window_layer, trl_controller->top_layer);		
+  layer_add_child(window_layer, trl_controller->bottom_layer);		
 }
 
 void trl_controller_unload(Controller* controller)
