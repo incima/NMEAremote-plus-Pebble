89446a3e34c272bf65f28cf06b0d85ac40d95d85
Holger Franzen <holger.franzen@incima.net>
2014-09-14 14:13:39 +0200

    Minor formatting changes.

--- src/common/fonts.h
+++ src/common/fonts.h
@@ -1,7 +1,7 @@
 /**
- * NMEAremote(c)+Pebble
+ * NMEAremote (c) + Pebble
  * 
- * Copyright (c) 2014 Holger Franzen aka incima
+ * Copyright (c) 2014 Holger Franzen for incima
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
