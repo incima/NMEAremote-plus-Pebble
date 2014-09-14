274716e58a980e01672cffbffc3f02affa2cda12
Holger Franzen <holger.franzen@incima.net>
2014-09-14 14:10:20 +0200

    Added version and mk

---
+++ README.mk
@@ -0,0 +1,7 @@
+NMEAremote © + Pebble
+=======================================
+
+Watchapp for the Pebble watch displaying data from an NMEAremote © app source.
+
+NMEAremote © Michael Zapf for zapfware.
+Available at: https://itunes.apple.com/de/app/nmearemote/id412806204?mt=8
